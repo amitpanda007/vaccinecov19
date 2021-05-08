b348745c8a5ba28651771fa0d0795b8dc2872d56
Amit Panda <amitpanda007@gmail.com>
2021-05-08 12:33:07 +0530

    filter fixes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,17 @@
-import { Component, Input, OnInit } from "@angular/core";
-import { formatDate } from "@angular/common";
-import { MatSnackBar } from "@angular/material";
-import { BehaviorSubject } from "rxjs";
-import { ErrorSnackbar, SuccessSnackbar } from "../common/snackbar.component";
-import { HomeService } from "../core/services/home.service";
+import { Component, Input, OnInit } from '@angular/core';
+import { formatDate } from '@angular/common';
+import { MatSnackBar } from '@angular/material';
+import { BehaviorSubject } from 'rxjs';
+import { ErrorSnackbar, SuccessSnackbar } from '../common/snackbar.component';
+import { HomeService } from '../core/services/home.service';
 
 @Component({
-  selector: "home",
-  templateUrl: "home.component.html",
-  styleUrls: ["home.component.scss"],
+  selector: 'home',
+  templateUrl: 'home.component.html',
+  styleUrls: ['home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public searchTerm: string = "";
+  public searchTerm: string = '';
   public readonlyMode: boolean = false;
   public showCardInfo = false;
   // centerSubject = new BehaviorSubject([]);
@@ -19,11 +19,11 @@ export class HomeComponent implements OnInit {
   public districts: any = [];
   public centerData: any = [];
   public filteredCenterData: any = [];
-
+  public districtId: any;
   public is18to44: boolean = false;
   public is45Plus: boolean = false;
   public isAvlSlots: boolean = false;
-
+  public isMultipleSelectable: boolean = true;
 
   constructor(
     private homeService: HomeService,
@@ -41,12 +41,15 @@ export class HomeComponent implements OnInit {
   }
 
   async findCenters() {
+    this.filteredCenterData = [];
+    this.centerData = [];
     console.log(this.searchTerm);
-    if(this.searchTerm.match(/^[1-9][0-9]{5}$/)) {
-      this.centerData  = await this.getCenterInfoByPin(this.searchTerm, "28");
-    }else {
+    if (this.searchTerm.match(/^[1-9][0-9]{5}$/)) {
+      this.centerData = await this.getCenterInfoByPin(this.searchTerm, '28');
+      this.filteredCenterData = this.centerData;
+    } else {
       this._snackBar.openFromComponent(ErrorSnackbar, {
-        data: "Please Check the provided PINCODE",
+        data: 'Please Check the provided PINCODE',
         duration: 3000,
       });
     }
@@ -56,20 +59,26 @@ export class HomeComponent implements OnInit {
     const locale = 'en-US';
     let centerList: any = [];
     try {
-      const weeks = Math.floor(days/7)
-      if(weeks == 0) {
+      const weeks = Math.floor(days / 7);
+      if (weeks == 0) {
         const today = new Date();
-        const newDate = formatDate(today, "dd-MM-yyyy", locale)
+        const newDate = formatDate(today, 'dd-MM-yyyy', locale);
         console.log(`Today: ${newDate}`);
         return await this.homeService.getCenterDetailsByPin(pincode, newDate);
-      }else {
-        for(let i = 0; i < weeks; i ++) {
+      } else {
+        for (let i = 0; i < weeks; i++) {
           const today = new Date();
-          var newFutureDate = new Date(today.setDate(today.getDate() + (7 * (i))));
-          const futureDate = formatDate(newFutureDate, "dd-MM-yyyy", locale);
+          var newFutureDate = new Date(today.setDate(today.getDate() + 7 * i));
+          const futureDate = formatDate(newFutureDate, 'dd-MM-yyyy', locale);
           console.log(`Today: ${futureDate}`);
-          let newCenters = await this.homeService.getCenterDetailsByPin(pincode, futureDate);
-          centerList = await this.aggregateCentersSessions(centerList, newCenters);
+          let newCenters = await this.homeService.getCenterDetailsByPin(
+            pincode,
+            futureDate
+          );
+          centerList = await this.aggregateCentersSessions(
+            centerList,
+            newCenters
+          );
         }
         return centerList;
       }
@@ -77,74 +86,88 @@ export class HomeComponent implements OnInit {
       console.log(error);
     }
   }
-  
+
   async aggregateCentersSessions(centerList, newCenters) {
-    let newList = []
-    if(centerList.length == 0) {
-      console.log("Adding New Data");
-      centerList = newCenters
-      return centerList
-    }else {
-      console.log("Already have data. adding sessions");
-      for(let i = 0; i < newCenters.centers.length; i++) {
-        for(let j = 0; j < centerList.centers.length; j++) {
+    let newList = [];
+    if (centerList.length == 0) {
+      console.log('Adding New Data');
+      centerList = newCenters;
+      return centerList;
+    } else {
+      console.log('Already have data. adding sessions');
+      for (let i = 0; i < newCenters.centers.length; i++) {
+        for (let j = 0; j < centerList.centers.length; j++) {
           const newCenter = newCenters.centers[i];
           const oldCenter = centerList.centers[j];
-          if(newCenter.center_id == oldCenter.center_id) {
-            centerList.centers[j].sessions = [...centerList.centers[j].sessions, ...newCenter.sessions];
+          if (newCenter.center_id == oldCenter.center_id) {
+            centerList.centers[j].sessions = [
+              ...centerList.centers[j].sessions,
+              ...newCenter.sessions,
+            ];
           }
         }
       }
-  
+
       // Check for newly added centers
       let oldCenterIds = [];
       let newCenterIds = [];
-      centerList.centers.forEach(oldCent => {
+      centerList.centers.forEach((oldCent) => {
         oldCenterIds.push(oldCent.center_id);
       });
-  
-      newCenters.centers.forEach(newCent => {
-        newCenterIds.push(newCent.center_id)
+
+      newCenters.centers.forEach((newCent) => {
+        newCenterIds.push(newCent.center_id);
       });
-  
-      let absentCenterIds = newCenterIds.filter(e=>!oldCenterIds.includes(e));
+
+      let absentCenterIds = newCenterIds.filter(
+        (e) => !oldCenterIds.includes(e)
+      );
       console.log(absentCenterIds);
-  
-      if(absentCenterIds.length > 0) {
-        absentCenterIds.forEach(id => {
-          newCenters.centers.forEach(cent => {
-            if(cent.center_id == id) {
-              console.log("Inserting New Center")
-              centerList.centers.push(cent)
+
+      if (absentCenterIds.length > 0) {
+        absentCenterIds.forEach((id) => {
+          newCenters.centers.forEach((cent) => {
+            if (cent.center_id == id) {
+              console.log('Inserting New Center');
+              centerList.centers.push(cent);
             }
-          })
-        })
+          });
+        });
       }
-  
-      return centerList
+
+      return centerList;
     }
   }
-  
+
   async getDistrictCenterInfo(districtId, days) {
     const locale = 'en-US';
     var centerList = [];
     var apiUrl;
     try {
-      const weeks = Math.floor(days/7)
-      if(weeks == 0) {
+      const weeks = Math.floor(days / 7);
+      if (weeks == 0) {
         const today = new Date();
-        const newDate = formatDate(today, "dd-MM-yyyy", locale)
+        const newDate = formatDate(today, 'dd-MM-yyyy', locale);
         console.log(`Today: ${newDate}`);
-        return await this.homeService.getCenterDetailsByDistrict(districtId, newDate);
-      }else {
-        for(let i = 0; i < weeks; i ++) {
+        return await this.homeService.getCenterDetailsByDistrict(
+          districtId,
+          newDate
+        );
+      } else {
+        for (let i = 0; i < weeks; i++) {
           console.log(i);
           const today = new Date();
-          var newFutureDate = new Date(today.setDate(today.getDate() + (7 * (i))));
-          const futureDate = formatDate(newFutureDate, "dd-MM-yyyy", locale)
+          var newFutureDate = new Date(today.setDate(today.getDate() + 7 * i));
+          const futureDate = formatDate(newFutureDate, 'dd-MM-yyyy', locale);
           console.log(`Today: ${futureDate}`);
-          let newCenters = await this.homeService.getCenterDetailsByDistrict(districtId, futureDate);
-          centerList = await this.aggregateCentersSessions(centerList, newCenters);
+          let newCenters = await this.homeService.getCenterDetailsByDistrict(
+            districtId,
+            futureDate
+          );
+          centerList = await this.aggregateCentersSessions(
+            centerList,
+            newCenters
+          );
         }
         return centerList;
       }
@@ -152,37 +175,47 @@ export class HomeComponent implements OnInit {
       console.log(error.response);
     }
   }
-  
+
   async filterByMinMaxAgeLimit(centerInfo, minAgeLimit, maxAgeLimit) {
-    const filteredCenter = centerInfo.centers.filter(cent => cent.sessions[0].min_age_limit >= minAgeLimit && cent.sessions[0].min_age_limit <= maxAgeLimit);
-    let finalData = {"centers": []};
+    const localCenterInfo = centerInfo;
+    const filteredCenter = localCenterInfo.centers.filter(
+      (cent) =>
+        cent.sessions[0].min_age_limit >= minAgeLimit &&
+        cent.sessions[0].min_age_limit <= maxAgeLimit
+    );
+    let finalData = { centers: [] };
     finalData.centers = [...filteredCenter];
     return finalData;
   }
-  
+
   async filterByMinAgeLimit(centerInfo, minAgeLimit) {
-    for(let i = 0; i < centerInfo.centers.length; i++) {
+    for (let i = 0; i < centerInfo.centers.length; i++) {
       const curCenter = centerInfo.centers[i];
-      if(curCenter.sessions[0].min_age_limit <= minAgeLimit) {
+      if (curCenter.sessions[0].min_age_limit <= minAgeLimit) {
         centerInfo.centers.splice(i, 1);
       }
     }
     return centerInfo;
   }
-  
+
   async filterByAvailableLimit(centerInfo, availableCapacity) {
-    const finalData = {"centers": []};
+    const localCenter = centerInfo;
+    const finalData = { centers: [] };
     const centerData = [];
-    centerInfo.centers.forEach(cent => {
-      const filteredSessions = cent.sessions.filter(sess => sess.available_capacity >= availableCapacity)
+    localCenter.centers.forEach((cent) => {
+      const filteredSessions = cent.sessions.filter(
+        (sess) => sess.available_capacity >= availableCapacity
+      );
       cent.sessions = filteredSessions;
       centerData.push(cent);
     });
     finalData.centers = [...centerData];
-  
+
     //Remove Empty session centers
-    const finalCenterList = finalData.centers.filter(centr => centr.sessions.length > 0) 
-    const finalCenterListNew = {"centers": []};
+    const finalCenterList = finalData.centers.filter(
+      (centr) => centr.sessions.length > 0
+    );
+    const finalCenterListNew = { centers: [] };
     finalCenterListNew.centers = [...finalCenterList];
     return finalCenterListNew;
   }
@@ -191,19 +224,82 @@ export class HomeComponent implements OnInit {
     this.showCardInfo = !this.showCardInfo;
   }
 
-  async onToggleChange() {
-    if(this.is18to44) {
-      this.centerData = await this.filterByMinMaxAgeLimit(this.centerData, "18", "44");
-    }
-    
-    if(this.is45Plus) {
-      this.centerData = await this.filterByMinMaxAgeLimit(this.centerData, "45", "50");
+  // async onToggleChange() {
+  //   if (this.is18to44) {
+  //     this.centerData = await this.filterByMinMaxAgeLimit(
+  //       this.centerData,
+  //       '18',
+  //       '44'
+  //     );
+  //   }
+
+  //   if (this.is45Plus) {
+  //     this.centerData = await this.filterByMinMaxAgeLimit(
+  //       this.centerData,
+  //       '45',
+  //       '50'
+  //     );
+  //   }
+
+  //   if (this.isAvlSlots) {
+  //     this.centerData = await this.filterByAvailableLimit(this.centerData, 1);
+  //   }
+  // }
+
+  async chipSelected(value: string) {
+    if (value === '18-44') {
+      if (this.is18to44) {
+        this.filteredCenterData = await this.filterByMinMaxAgeLimit(
+          this.centerData,
+          '18',
+          '44'
+        );
+      } else {
+        this.filteredCenterData = this.centerData;
+      }
     }
 
-    if(this.isAvlSlots) {
-      this.centerData = await this.filterByAvailableLimit(this.centerData, 1);
+    if (value === '45+') {
+      if (this.is45Plus) {
+        this.filteredCenterData = await this.filterByMinMaxAgeLimit(
+          this.centerData,
+          '45',
+          '50'
+        );
+      } else {
+        this.filteredCenterData = this.centerData;
+      }
     }
 
+    if (value === 'ALL') {
+      if (this.is18to44 || this.is45Plus) {
+        if (this.isAvlSlots) {
+          this.filteredCenterData = await this.filterByAvailableLimit(
+            this.filteredCenterData,
+            1
+          );
+        } else {
+          if (this.searchTerm) {
+            await this.findCenters();
+          } else {
+            this.gatherDistrictData();
+          }
+        }
+      } else {
+        if (this.isAvlSlots) {
+          this.filteredCenterData = await this.filterByAvailableLimit(
+            this.centerData,
+            1
+          );
+        } else {
+          if (this.searchTerm) {
+            await this.findCenters();
+          } else {
+            this.gatherDistrictData();
+          }
+        }
+      }
+    }
   }
 
   async stateSelected($event) {
@@ -214,7 +310,17 @@ export class HomeComponent implements OnInit {
   }
 
   async districtSelected($event) {
-    const districtId = $event.value;
-    this.centerData = await this.getDistrictCenterInfo(districtId, "28");
+    this.filteredCenterData = [];
+    this.centerData = [];
+    this.districtId = $event.value;
+    this.centerData = await this.getDistrictCenterInfo(this.districtId, '28');
+    this.filteredCenterData = this.centerData;
+  }
+
+  async gatherDistrictData() {
+    if (this.districtId) {
+      this.centerData = await this.getDistrictCenterInfo(this.districtId, '28');
+      this.filteredCenterData = this.centerData;
+    }
   }
 }
